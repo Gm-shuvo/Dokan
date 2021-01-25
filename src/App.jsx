593c9661fcbe87adeed336a8bdc2e28ae5a2b3f0
Gm-shuvo <gm.shuvo.ru.cse@gmail.com>
2021-01-25 15:13:06 +0600

    fetching products from Commerce.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
-import React from 'react'
-//import Navigation from '../src/component/navigation/Navigation'
+import React,{useState,useEffect} from 'react'
 import './App.css';
+import { commerce } from "./lib/commerce";
 import {Products,Navbar} from '../src/component'
 const App =() =>{
+  const [products, setproducts] = useState([])
+
+  const fetchData = async()=>{
+    const {data} = await commerce.products.list()
+    setproducts(data)
+  }
+  useEffect(() => {
+    fetchData()
+  }, [])
+
   return (
     <div className="app">
         <Navbar/>
-       <Products/>
+       <Products />
     </div>
   );
 }
