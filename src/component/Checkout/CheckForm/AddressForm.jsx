5bcfb11d08e6fd38f81901b88f6a454339669ae4
Gm-shuvo <gm.shuvo.ru.cse@gmail.com>
2021-02-02 16:50:36 +0600

    Adding Subdivions but error occur in fetching shipping options

--- src/component/Checkout/CheckForm/AddressForm.jsx
+++ src/component/Checkout/CheckForm/AddressForm.jsx
@@ -14,17 +14,52 @@ const AddressForm = ({checkoutToken}) => {
 	const [shippingOption, setshippingOption] = useState('')
 
 	const methods = useForm()
-	
-	console.log(checkoutToken.id);
+	const countries = Object.entries(shippingCountries).map(([code,name])=>({id:code ,label:name})) 
+	const subdivisions = Object.entries(shippingSubdivisions).map(([code,name])=>({id:code ,label:name})) 
+	//console.log(checkoutToken.id);
+	// const Options = shippingOptions.map((option)=>)
+
 	const fetchShippingCountries = async (checkoutTokenId) =>{
-		const {countries} = await commerce.services.localeListShippingCountries(checkoutTokenId)
-		console.log(countries);
-		setshippingCountries(countries)
+		try {
+			const {countries} = await commerce.services.localeListCountries(checkoutTokenId)
+			//console.log(countries);
+			setshippingCountries(countries)
+		} catch (error) {
+			console.log({message:'Fechting Countries error'});
+		}
 	}
 
+
+	const fetchingShippingSubdivisions =async(shippingCountry)=>{
+		try {
+			const {subdivisions}= await commerce.services.localeListSubdivisions(shippingCountry)
+			//console.log(subdivisions);
+			setshippingSubdivisions(subdivisions)
+		} catch (error) {
+			console.log({message:'Fechting Subdivisions of Country error'});
+		}
+	}
+	const fetchingShippingOptions =async(checkoutTokenId,country = 'US')=>{
+		try {
+			const Options = await commerce.checkout.getShippingOptions(checkoutTokenId,{country})
+			console.log(Options);
+			setshippingOptions(Options)
+		} catch (error) {
+			console.log({message:'Fechting ShippingOptions of Country error'});
+		}
+	}
+	console.log(shippingCountry);
 	useEffect(() => {
 		fetchShippingCountries(checkoutToken.id)
-	}, [checkoutToken])
+	}, [])
+	
+	useEffect(() => {
+		fetchingShippingSubdivisions(shippingCountry)
+	}, [shippingCountry])
+
+	useEffect(() => {
+		if(shippingSubdivision)	fetchingShippingOptions(checkoutToken.id)
+	}, [shippingSubdivision])
 
     return (
         <>
@@ -41,14 +76,32 @@ const AddressForm = ({checkoutToken}) => {
 						<FormInput required name='city' label='City'/>
 						<FormInput required name='zip' label='Zip/Postal code'/>
 
-						{/* <Grid item xs={12 } sm={6}>
+						<Grid item xs={12 } sm={6}>
 							<InputLabel>Shipping Country</InputLabel>
-							<Select value={''} fullWidth onChange={''}>
-								<MenuItem key={''} value={''}>
-									select me
-								</MenuItem>
+							<Select value={shippingCountry} fullWidth onChange={(e)=>setshippingCountry(e.target.value)}>
+								{countries.map((country)=>(
+									<MenuItem key={country.id} value={country.id}>{country.label}</MenuItem>
+								))}
+							</Select>
+						</Grid>
+						<Grid item xs={12 } sm={6}>
+							<InputLabel>Shipping Subdivisions</InputLabel>
+							<Select value={shippingSubdivision} fullWidth onChange={(e)=>setshippingSubdivision(e.target.value)}>
+								{subdivisions.map((division)=>(
+									<MenuItem key={division.id} value={division.id}>{division.label}</MenuItem>
+								))}
+							</Select>
+						</Grid>
+
+						{/* <Grid item xs={12 } sm={6}>
+							<InputLabel>Shipping Subdivisions</InputLabel>
+							<Select value={shippingSubdivision} fullWidth onChange={(e)=>setshippingSubdivision(e.target.value)}>
+								{subdivisions.map((division)=>(
+									<MenuItem key={division.id} value={division.id}>{division.label}</MenuItem>
+								))}
 							</Select>
 						</Grid> */}
+						
                     </Grid>
                 </form>
             </FormProvider>
